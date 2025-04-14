975daf5c62071008acb51a54ade1191104ea80b0
Rubenspv <rubenpv011992@gmail.com>
2025-04-14 22:31:19 +0200

    chore: changed environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://chatbot-microservice.vercel.app/api/chat',
+  apiUrl: 'https://assistant.rpvai.com/api/chat',
   dIdClientKey: 'Z29vZ2xlLW9hdXRoMnwxMTAyNTE5MzEwNDc0NjA1MDk0ODc6YnJ5N2t3VXZzRFhHSHhyd1hINk9S',
   dIdAgentId: 'agt_xxmFLzST'
 };
